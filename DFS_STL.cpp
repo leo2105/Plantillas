25dc5e2555cde6535e415d601284ae7ae96616af
leo2105 <lleonv@uni.pe>
2016-07-23 03:08:23 -0500

    Create DFS_STL.cpp

---
+++ DFS_STL.cpp
@@ -0,0 +1,31 @@
+// c++11 ...  g++ DFS_STL.g++ -std=c++11
+
+#include <bits/stdc++.h>
+#define FOR(it,A) for(auto it = A.begin(); it != A.end(); it++)
+#define N 100
+#define E 6    // Aristas
+using namespace std;
+
+int vis[N];
+vector<int> G[N];
+
+void dfs(int s){
+
+	if(vis[s]) return;
+	
+	vis[s] = true;
+	FOR(it,G[s])
+		dfs(*it);
+}
+int main(){
+
+	int a,b;
+	for(int i=0;i<E;i++){
+		cin>>a>>b;
+		G[a].push_back(b);
+		G[b].push_back(a);
+	}
+  cin>>start;
+	dfs(0);
+	return 0;
+}
